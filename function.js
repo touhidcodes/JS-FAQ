839e85b7e3e2f2c6bf2d888bbda5e99ec876e6d3
touhidcodes <touhidcodes@gmail.com>
2022-09-30 19:56:33 +0600

    even odd

--- function.js
+++ function.js
@@ -1,6 +1,11 @@
+// Even Odd Function
 number = [5, 23, 45, 76, 56, 78, 556, 53];
 
 for (let i = 0; i < number.length; i++) {
   const num = number[i];
-  console.log(num);
+  if (num % 2 == 0) {
+    console.log(num, ": is even number");
+  } else {
+    console.log(num, ": is odd number");
+  }
 }
