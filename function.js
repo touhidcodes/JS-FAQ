aa1631ca4d40f9fa46acf5491be6f8f678dcfe94
touhidcodes <touhidcodes@gmail.com>
2022-09-30 20:24:39 +0600

    function complete

--- function.js
+++ function.js
@@ -1,5 +1,4 @@
 // Even/Odd For loop Function
-nums = [5, 23, 45, 76, 56, 78, 556, 53];
 
 function evenify_all(nums) {
   for (let i = 0; i < nums.length; i++) {
@@ -11,3 +10,9 @@ function evenify_all(nums) {
     }
   }
 }
+
+let nums = [5, 23, 45, 76, 56, 78, 556, 53];
+evenify_all(nums);
+
+let friends_age = [13, 20, 19, 15, 17, 16, 14, 18];
+evenify_all(friends_age);
