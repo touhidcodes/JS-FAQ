7b35e89ce8c55b33e051d09d38c3812be3541737
touhidcodes <touhidcodes@gmail.com>
2022-10-01 22:05:28 +0600

    callback function

---
+++ callback.js
@@ -0,0 +1,21 @@
+function callback_exp(name, age, task) {
+  console.log("My Name is ", name);
+  console.log("My age ", age);
+  task();
+}
+
+function washHand() {
+  console.log("Wash your hand Properly");
+}
+
+function takeBath() {
+  console.log("Take a bath Properly");
+}
+
+function scroollFB() {
+  console.log("Scrolling Facebook");
+}
+
+callback_exp("Rohim Khan", 34, washHand);
+callback_exp("Korimm Khan", 29, takeBath);
+callback_exp("Bolim Khan", 16, scroollFB);
